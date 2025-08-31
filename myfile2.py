7f3f781711a14bc4e3897006668daba1caa94c02
mamsifam <mamsifam@gmail.com>
2025-08-31 20:52:32 +0530

    add another label

--- myfile2.py
+++ myfile2.py
@@ -4,5 +4,7 @@ window =tk.Tk()
 window.title('git hub test window')
 label = tk.Label(window,text='Devalop')
 label.pack(padx=10,pady=10)
+label2 = tk.Label(window,text='Devalop2')
+label2.pack(padx=15,pady=15)
 
 window.mainloop()
