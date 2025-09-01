23fa5d0ad3fc6482d280856d102a546b08c06301
mamsifam <mamsifam@gmail.com>
2025-09-01 20:38:13 +0530

    add new button

--- myfile2.py
+++ myfile2.py
@@ -6,5 +6,7 @@ label = tk.Label(window,text='Devalop')
 label.pack(padx=10,pady=10)
 label2 = tk.Label(window,text='Devalop2')
 label2.pack(padx=15,pady=15)
+button = tk.button(window,text ='click here')
+button.pack()
 
 window.mainloop()
